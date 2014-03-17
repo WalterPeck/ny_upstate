f569e9a87c6331c56f7c66ec710580659ca28839
WalterPeck <chrislabarge2@gmail.com>
2014-03-16 23:49:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NyUpstate::Application.config.secret_key_base = '38b471c50f8af10076a9cfa4e5195480677131570b2a0aa00ff43f4c535e5f93f8d8fa647c0b95df3ab7b981d8c8774fb4a001def1a8249a1d621a3642aff7b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NyUpstate::Application.config.session_store :cookie_store, key: '_ny_upstate_session'
